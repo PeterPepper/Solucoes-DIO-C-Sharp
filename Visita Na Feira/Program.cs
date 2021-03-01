89171beed113b79ab9fbcd8926f5a38b46b31a56
Pedro Pimenta <pedropimenta@rocketmail.com>
2021-03-01 17:50:40 -0300

    First commit

---
+++ Visita Na Feira/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Visita_Na_Feira
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, x;
+
+
+            string input = Console.ReadLine();
+
+            a = Convert.ToInt32(input.Split(" ")[0]);
+            b = Convert.ToInt32(input.Split(" ")[1]);
+
+            x = a + b; //complete o código de acordo com o cálculo esperado
+
+            Console.WriteLine("X = {0}", x);
+        }
+    }
+}
